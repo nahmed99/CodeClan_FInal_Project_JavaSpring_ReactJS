1ab719eb81b91b3badd6e5a89b63750bf021fb6c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-20 12:25:13 +0000

    CustomerService (for http requests) coded on client side. Tested update method

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import {getAllCustomers, updateCustomer, deleteCustomer} from './Services/CustomerService';
 
 function App() {
+
+  const [customers, setCustomers] = useState([]);
+
+
+  useEffect(() => {
+    getAllCustomers()
+    .then((customers) => {
+      setCustomers(customers);
+      console.log(customers);
+    })
+  }, []);
+
+
+  const onHandleUpdate = () => {
+    customers[0].firstName = "Awite Yaara";
+    // setCustomers(customers); This is setting up the local data
+    updateCustomer(customers[0]); // This is updating the server-side data.
+  }
+
+
+  const onHandleDelete = () => {
+    deleteCustomer(customers[0].id); // This is updating the server-side data.
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p onClick={onHandleUpdate}> Click to update first customer </p>
+      <p onClick={onHandleDelete}> Click to delete first customer </p>
     </div>
   );
 }
---
+++ client/src/Services/CustomerService.js
@@ -0,0 +1,42 @@
+const baseURL = 'http://localhost:8080/customers/';
+
+// Retrieve ALL records
+export const getAllCustomers = () => {
+    return fetch(baseURL)
+    .then(res => res.json())
+}
+
+// Retrieve ONE record
+export const getCustomer = (id) => {
+    return fetch(baseURL + id)
+    .then(res => res.json())
+}
+
+// Create record
+export const addCustomer = (customer) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(customer),
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+}
+
+// Update record
+export const updateCustomer = (customer) => {
+    return fetch(baseURL + customer.id, {    // Do we need to send an id to Spring???
+        method: "PATCH",
+        body: JSON.stringify(customer),
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json());
+}
+
+// Delete record
+export const deleteCustomer = (id) => {
+    return fetch(baseURL + id, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'}
+    });
+}
+
