aca8bb9b51fa8aa7d07737525ab2e43a6a8a847e
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-22 11:45:52 +0000

    Added background color to login link on navbar

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -4,8 +4,15 @@ const Navbar = ({loggedIn}) => {
             <h1>Do It Yourself - SELL IT!</h1>
             <div className="links">
                 <a href="/">Home</a>
-                {!loggedIn && <a href="/login">Login</a>}
+
+                {!loggedIn && <a href="/login" style={{
+                    color: "darkblue",
+                    backgroundColor: "powderblue",
+                    borderRadius: "5px"
+                }}>Login</a>}
+                
                 {loggedIn && <a href="/user">User Profile</a>}
+
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
             </div>
