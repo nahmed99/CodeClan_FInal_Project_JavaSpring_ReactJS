afd21da8f20712f3ad696098fe8d03139d10d46c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-23 16:26:18 +0000

    minor changes made

--- client/src/components/UpdateCarAdvert.js
+++ client/src/components/UpdateCarAdvert.js
@@ -33,31 +33,40 @@ const UpdateCarAdvert = () => {
         <form className="stock-form" onSubmit={handleSubmit}>
 
             <label htmlFor="title">Title: </label>
-            <input type="text" id="title" name="title" value={data.state["title"]}/>
+            <textarea type="text" id="title" name="title" value={data.state["title"]}
+            required />
 
             <label htmlFor="description">Description: </label>
-            <input type="text" id="description" name="description" value={data.state["description"]} />
+            <textarea type="text" id="description" name="description" value={data.state["description"]} required />
 
             <label htmlFor="make">Make: </label>
-            <input type="text" id="make" name="make" value={data.state["make"]}/>
+            {/* <input type="text" id="make" name="make" value={data.state["make"]}required /> */}
+            <select required >
+                <option selected="selected">{data.state["make"]}</option>
+                <option value="audi">Audi</option>
+                <option value="ford">Ford</option>
+                <option value="toyota">Toyota</option>
+                <option value="vauxhall">Vauxhall</option>
+                <option value="volkswagon">Volkswagon</option>
+            </select>
 
             <label htmlFor="model">Model: </label>
-            <input type="text" id="model" name="model" value={data.state["model"]}/>
+            <input type="text" id="model" name="model" value={data.state["model"]} required />
 
             <label htmlFor="registration">Registration Year: </label>
-            <input type="text" id="registration" name="registration" value={data.state["regYear"]}/>
+            <input type="text" id="registration" name="registration" value={data.state["regYear"]} required />
 
             <label htmlFor="seats">Seats: </label>
-            <input type="text" id="seats" name="seats" value={data.state["numSeats"]}/>
+            <input type="text" id="seats" name="seats" value={data.state["numSeats"]} required />
 
             <label htmlFor="doors">Doors: </label>
-            <input type="text" id="doors" name="doors" value={data.state["numDoors"]}/>
+            <input type="text" id="doors" name="doors" value={data.state["numDoors"]} required />
 
             <label htmlFor="colour">Colour: </label>
-            <input type="text" id="colour" name="colour" value={data.state["colour"]}/>
+            <input type="text" id="colour" name="colour" value={data.state["colour"]} required />
 
             <label htmlFor="price">Price: </label>
-            <input type="text" id="price" name="price" value={data.state["price"]}/>
+            <input type="text" id="price" name="price" value={data.state["price"]} required />
 
             <input
                 type="submit"
